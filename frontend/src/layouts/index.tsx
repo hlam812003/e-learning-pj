03f62874e09f78381ce0958fd8065f6f76b3ae13
trongdung07004 <trongdung143@gmail.com>
2025-03-24 17:49:34 +0700

    update courses

--- frontend/src/layouts/index.tsx
+++
@@ -1,7 +0,0 @@
-import MainLayout from './MainLayout'
-import AuthLayout from './AuthLayout'
-
-export {
-  MainLayout,
-  AuthLayout
-}
--- frontend/src/pages/public/auth/index.tsx
+++
@@ -1,7 +0,0 @@
-import Login from './Login'
-import Register from './Register'
-
-export { 
-  Login as LoginPage, 
-  Register as RegisterPage 
-}
