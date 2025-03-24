524f6e4382429bd0eb74abc643df65eb89e956c6
Lưu Trọng Dũng <119412611+trongdung143@users.noreply.github.com>
2025-03-24 17:45:28 +0700

    Delete AI directory

--- AI/scripts/load_model.py
+++
@@ -1,8 +0,0 @@
-from langchain_together import Together
-
-llm = Together(
-    model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
-    temperature=0.2,
-    together_api_key="96a333ce4132218457ff6840c5a7a6a6d5ebf6eb2d3125e87ef46f5e74e44303",
-    max_tokens=512,
-)
