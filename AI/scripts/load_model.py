dd082897a6e0e52f16cefa2abba47c2c45f8fba5
trongdung07004 <trongdung143@gmail.com>
2025-03-28 11:13:08 +0700

    update requirements.txt Dockerfile

--- AI/scripts/load_model.py
+++ AI/scripts/load_model.py
@@ -11,5 +11,5 @@ llm2 = Together(
     model="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
     temperature=0.8,
     together_api_key="96a333ce4132218457ff6840c5a7a6a6d5ebf6eb2d3125e87ef46f5e74e44303",
-    max_tokens=2048,
+    max_tokens=1024,
 )
