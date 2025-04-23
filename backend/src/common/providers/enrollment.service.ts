f3c065d5c0b51e66015639add8d1d24b51af0bb1
HuyHoang04 <lahuyhoang04@gmail.com>
2025-04-23 12:12:47 +0700

    pre for deloy

--- backend/src/common/providers/enrollment.service.ts
+++ backend/src/common/providers/enrollment.service.ts
@@ -1,5 +1,9 @@
 // enrollment.service.ts
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { EnrollmentDAO } from '../DAO/enrollment.dao';
 import { CreateEnrollmentInput } from '../DTO/enrollment/enrollment.input';
 import { EnrollmentResponse } from '../DTO/enrollment/enrollment.response';
@@ -8,7 +12,9 @@ import { EnrollmentResponse } from '../DTO/enrollment/enrollment.response';
 export class EnrollmentService {
   constructor(private readonly enrollmentDAO: EnrollmentDAO) {}
 
-  async enrollUserToCourse(input: CreateEnrollmentInput): Promise<EnrollmentResponse> {
+  async enrollUserToCourse(
+    input: CreateEnrollmentInput,
+  ): Promise<EnrollmentResponse> {
     const { userId, courseId, totalLessons } = input;
 
     const [user, course] = await Promise.all([
@@ -17,12 +23,20 @@ export class EnrollmentService {
     ]);
 
     if (!user) throw new NotFoundException(`User with ID ${userId} not found`);
-    if (!course) throw new NotFoundException(`Course with ID ${courseId} not found`);
+    if (!course)
+      throw new NotFoundException(`Course with ID ${courseId} not found`);
 
-    const existingEnrollment = await this.enrollmentDAO.findEnrollment(userId, courseId);
-    if (existingEnrollment) throw new ConflictException('User already enrolled in this course');
+    const existingEnrollment = await this.enrollmentDAO.findEnrollment(
+      userId,
+      courseId,
+    );
+    if (existingEnrollment)
+      throw new ConflictException('User already enrolled in this course');
 
-    const enrollment = await this.enrollmentDAO.createEnrollment(userId, courseId);
+    const enrollment = await this.enrollmentDAO.createEnrollment(
+      userId,
+      courseId,
+    );
     await this.enrollmentDAO.createProgress(userId, courseId, totalLessons);
 
     return enrollment;
@@ -30,7 +44,10 @@ export class EnrollmentService {
 
   async getUserEnrollments(userId: string): Promise<EnrollmentResponse[]> {
     const enrollments = await this.enrollmentDAO.getUserEnrollments(userId);
-    if (!enrollments.length) throw new NotFoundException(`No enrollments found for user ID: ${userId}`);
+    if (!enrollments.length)
+      throw new NotFoundException(
+        `No enrollments found for user ID: ${userId}`,
+      );
 
     return enrollments.map((enrollment) => ({
       userId: enrollment.userId,
@@ -39,4 +56,3 @@ export class EnrollmentService {
     }));
   }
 }
-
--- backend/src/modules/message.module.ts
+++ backend/src/modules/message.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MessageService } from '../common/providers/message.service';
 import { MessageDAO } from '../common/DAO/message.dao';
 import { MessageResolver } from '../common/resolvers/message.resolver';
-import { ConversationService } from '../common/providers/conversation.service';
-import { ConversationDAO } from '../common/DAO/conversation.dao';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { AuthModule } from './auth.module';
 import { HttpModule } from '@nestjs/axios';
+import { PubSub } from 'graphql-subscriptions';
+import { ConversationService } from '../common/providers/conversation.service';
+import { ConversationDAO } from '../common/DAO/conversation.dao';
 
 @Module({
   imports: [PrismaModule, AuthModule, HttpModule],
@@ -16,6 +17,10 @@ import { HttpModule } from '@nestjs/axios';
     MessageResolver,
     ConversationService,
     ConversationDAO,
+    {
+      provide: 'PUB_SUB',
+      useValue: new PubSub(),
+    },
   ],
   exports: [MessageService],
 })
