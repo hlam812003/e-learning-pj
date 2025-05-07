d6504b8d79a87287053d37ecf0187b091904059f
HWAN <68631787+hlam812003@users.noreply.github.com>
2025-05-07 09:00:55 +0700

    feat: add text below loading spinner

--- frontend/src/components/common/layouts/Loading.tsx
+++ frontend/src/components/common/layouts/Loading.tsx
@@ -2,10 +2,11 @@ import './Loading.css'
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
+    <div className="fixed inset-0 flex flex-col gap-3 items-center justify-center">
       <svg viewBox="25 25 50 50" className="loading__svg">
         <circle r="20" cy="50" cx="50" className="loading__circle" />
       </svg>
+      <span className="text-black text-[1.15rem] font-medium">Loading...</span>
     </div>
   )
 }
