083ef76bd48e5271174df26febbe5a096efffdd9
HWAN <68631787+hlam812003@users.noreply.github.com>
2025-05-08 21:39:19 +0700

    feat: testing new optimize

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,76 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    visualizer({
+      open: false,
+      // gzipSize: true,
+      brotliSize: true
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress', // gzip
+      ext: '.br',
+      threshold: 10240
+    })
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1600,
+    target: 'esnext',
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn']
+      },
+      output: {
+        comments: false
+      }
     },
+    rollupOptions: {
+      treeshake: 'recommended',
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+          if (id.includes('/features/')) {
+            const feature = id.split('/features/')[1].split('/')[0]
+            return `feature-${feature}`
+          }
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'react', 
+      'react-dom', 
+      'react-router-dom',
+      'three',
+      '@react-three/fiber',
+      '@react-three/drei'
+    ],
+    esbuildOptions: {
+      target: 'esnext'
+    }
   }
 })
